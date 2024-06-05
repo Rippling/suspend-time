98ab2165b17e3ba45ba8b441b2399d8bdbbcb36e
Calder White <calwhite@rippling.com>
2024-06-05 16:35:46 -0700

    feat: Update main.rs example.

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use std::time::Instant;
+
+use suspend_time::SuspendUnawareInstant;
+
+fn main() {
+    let t1 = Instant::now();
+
+    let z = SuspendUnawareInstant::now();
+
+    let duration = t1.elapsed();
+
+    // dummy print to get the optimizer to keep z around
+    println!("dummy print: {:#?}", z.elapsed());
+
+    println!("Took: {:#?}", duration);
+}
