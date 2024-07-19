dce5699f170cc70aae66a042f551ab8ceb6a1d12
Calder White <calwhite@rippling.com>
2024-07-19 16:39:42 -0700

    Update public docs for crates.io + docs.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,39 @@
-//! A library to solve platform inconsistencies in the standard time library,
-//! specifically surrounding system suspension.  See [`SuspendUnawareInstant`]
-//! for more details
+//! Suspend-time is a cross-platform monotonic clock that is suspend-unaware, written in Rust!
+//! It allows system suspension (e.g. when a user closes their laptop on windows) to not affect
+//! `Instant` durations and timeouts!
+//!
+//! Example of using [`SuspendUnawareInstant`]:
+//! ```
+//! use std::{thread, time};
+//! use suspend_time::{SuspendUnawareInstant};
+//!
+//! fn main() {
+//!     // If you used std::time::Instant here and you suspend the system on windows,
+//!     // it will print that more than 3 seconds (circa July 2024).
+//!     // With SuspendUnawareInstant this has no effect.
+//!     let instant = SuspendUnawareInstant::now();
+//!     let three_secs = time::Duration::from_secs(3);
+//!     thread::sleep(three_secs);
+//!     println!("{:#?}", instant.elapsed());
+//! }
+//! ```
+//!
+//! Example of using `suspend_time::`[`timeout`]
+//!
+//! ```
+//! use std::time::Duration;
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     // If you suspend the system during main's execution, Tokio will time
+//!     // out even though it only slept for 1 second. suspend_time::timeout does not.
+//!     let _ = suspend_time::timeout(
+//!         Duration::from_secs(2),
+//!         suspend_time::sleep(Duration::from_secs(1)),
+//!     ).await;
+//! }
+//! ```
+//!
 use std::{
     error::Error,
     fmt,
@@ -197,6 +230,7 @@ impl fmt::Display for TimedOutError {
 
 impl Error for TimedOutError {}
 
+/// The same API as tokio::time::timeout, except it is dependant on SuspendUnawareInstant for measuring time.
 pub async fn timeout<'a, F>(duration: Duration, future: F) -> Result<F::Output, TimedOutError>
 where
     F: Future + 'a,
@@ -211,6 +245,7 @@ where
     }
 }
 
+/// The same API as tokio::time::sleep, except it is dependant on SuspendUnawareInstant for measuring time.
 pub async fn sleep(duration: Duration) {
     let deadline = SuspendUnawareInstant::now() + duration;
     let mut now = SuspendUnawareInstant::now();
